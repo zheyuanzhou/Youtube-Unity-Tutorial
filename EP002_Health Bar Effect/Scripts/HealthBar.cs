dd0abfeafb31af243036f80501053e0c7a9c4f72
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-03-15 21:11:05 -0400

    Add files via upload

---
+++ EP002_Health Bar Effect/Scripts/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Image healthPointImage;
+    public Image healthPointEffect;
+
+    private PlayerControl player;
+    [SerializeField] private float hurtSpeed = 0.003f;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+    }
+
+    private void Update()
+    {
+        healthPointImage.fillAmount = player.currentHp / player.maxHp;
+
+        if(healthPointEffect.fillAmount >= healthPointImage.fillAmount)
+        {
+            healthPointEffect.fillAmount -= hurtSpeed;
+        }
+        else
+        {
+            healthPointEffect.fillAmount = healthPointImage.fillAmount;
+        }
+    }
+
+}
---
+++ EP002_Health Bar Effect/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float maxHp = 100.0f;
+    public float currentHp = 10.0f;
+
+    private void Awake()
+    {
+        currentHp = maxHp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("I have hitted" + collision);
+        currentHp -= 15.0f;
+        Debug.Log("Current Hp is " + currentHp);
+    }
+
+}
