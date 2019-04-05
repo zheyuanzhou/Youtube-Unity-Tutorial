db6b2202ae8ae2fb06ea1ef310bb24d718d0c8e3
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-04-05 01:40:23 -0400

    Create Card.cs

---
+++ EP06_02 Edit Card with Class/Card.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+
+
+    public int cardMana;
+    public int cardAttack;
+    public int cardHealth;
+
+    public string cardName;
+    [TextArea(1, 3)]
+    public string cardDescription;
+
+    public Sprite cardSprite;
+
+    //step 2 cardBackground
+    
+
+    //step 3 cardClass
+
+
+}
