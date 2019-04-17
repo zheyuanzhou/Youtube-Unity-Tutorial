912f837a995aec0c25bfafe9afd9a405a8c2f832
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-04-17 03:55:05 -0400

    Add files via upload

---
+++ EP06_04 Use Enum to add Card Color&Class/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+
+
+    public int cardMana;
+    public int cardAttack;
+    public int cardHealth;
+
+    public string cardName;
+    [TextArea(1, 3)]
+    public string cardDescription;
+
+    public Sprite cardSprite;
+
+    //step 2 cardBackground
+    public Sprite cardBackground;
+
+    //step 3 cardClass
+
+    public enum CardClass
+    {
+        Human,
+        Orc,
+        UnDead,
+        NightElf
+    }
+
+    public CardClass cardClass;
+
+}
---
+++ EP06_04 Use Enum to add Card Color&Class/CardDisplay.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public Card[] cards;
+    public GameObject[] gameCards;
+
+    public Text manaText;
+    public Text attackText;
+    public Text healthText;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image cardImage;
+    public Image cardBackgroundImage;
+
+    public Text cardClass;
+
+    public Color humanColor;
+    public Color orcColor;
+    public Color undeadColor;
+    public Color nightElfColor;
+
+    //TODO CardColor
+
+    private void Awake()
+    {
+        for(int i =0; i < cards.Length; i++)
+        {
+            gameCards[i].transform.Find("Mana").GetChild(0).GetComponent<Text>().text = cards[i].cardMana.ToString();
+            gameCards[i].transform.Find("Attack").GetChild(0).GetComponent<Text>().text = cards[i].cardAttack.ToString();
+            gameCards[i].transform.Find("Health").GetChild(0).GetComponent<Text>().text = cards[i].cardHealth.ToString();
+            gameCards[i].transform.Find("NameBoard").GetChild(0).GetComponent<Text>().text = cards[i].cardName;
+            gameCards[i].transform.Find("DescriptionBoard").GetChild(0).GetComponent<Text>().text = cards[i].cardDescription;
+            gameCards[i].transform.Find("CardProfile_Mask").GetChild(1).GetComponent<Image>().sprite = cards[i].cardSprite;
+            gameCards[i].transform.Find("CardProfile_Mask").GetChild(0).GetComponent<Image>().sprite = cards[i].cardBackground;
+
+            gameCards[i].transform.Find("Class").GetChild(0).GetComponent<Text>().text = cards[i].cardClass.ToString();
+        
+            if(cards[i].cardClass.ToString() == "Human")
+            {
+                gameCards[i].GetComponent<Image>().color = humanColor;
+            }
+            if (cards[i].cardClass.ToString() == "Orc")
+            {
+                gameCards[i].GetComponent<Image>().color = orcColor;
+            }
+            if (cards[i].cardClass.ToString() == "UnDead")
+            {
+                gameCards[i].GetComponent<Image>().color = undeadColor;
+            }
+            if (cards[i].cardClass.ToString() == "NightElf")
+            {
+                gameCards[i].GetComponent<Image>().color = nightElfColor;
+            }
+
+        }
+    }
+
+
+    //MARKER Episode Single Card Creation
+    //manaText.text = cards[0].cardMana.ToString();
+    //attackText.text = cards[0].cardAttack.ToString();
+    //healthText.text = cards[0].cardHealth.ToString();
+
+    //cardImage.sprite = cards[0].cardSprite;
+    //nameText.text = cards[0].cardName;
+    //descriptionText.text = cards[0].cardDescription;
+
+    //cardBackgroundImage.sprite = cards[0].cardBackground;
+
+}
