393abe022bf2ccc6af90a6b8c577bc5dcd35351e
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-06-08 15:37:15 -0400

    Add files via upload

---
+++ EP08_Tilemap/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-6.31/11.6/-7.2
+public class Camera : MonoBehaviour
+{
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(
+        Mathf.Clamp(target.position.x, -6.3f, 6.3f),
+        Mathf.Clamp(target.position.y, -7.2f, 11.6f),
+        transform.position.z);
+    }
+
+}
---
+++ EP08_Tilemap/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        float moveV = Input.GetAxis("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
