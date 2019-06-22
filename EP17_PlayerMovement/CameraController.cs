81aa3de1625c050c7ff9a4e92869fb2a1a776d4a
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-06-22 13:36:58 -0400

    Add files via upload

---
+++ EP17_PlayerMovement/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float smoothSpeed;
+
+    [SerializeField] private float minX, maxX, minY, maxY;
+
+    private void Start()
+    {
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();//MARKER use tag to find target
+        target = PlayerController.instance.transform;//MARKER using singleton way to get target
+    }
+
+    private void LateUpdate()
+    {
+        //MARKER Method 1 tradition method of move camera MARKER FOLLOW TARGET(PLAYER)
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);//transform.position : camera current position 
+
+        //MARKER smoothly move Camera
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), smoothSpeed * Time.deltaTime);
+
+        //MARKER Limit the Range
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX),//x limit range
+                                         Mathf.Clamp(transform.position.y, minY, maxY),//y limit range
+                                         transform.position.z);
+    }
+
+}
---
+++ EP17_PlayerMovement/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //CORE Singleton mode
+    public static PlayerController instance;//MARKER KEEp in the different scene
+
+    private Rigidbody2D rb;
+    public float moveH, moveV;
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    public string scenePassword;//MARKER store one name when the plar leave for another scene
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
