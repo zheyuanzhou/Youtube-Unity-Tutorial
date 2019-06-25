fb037fe687ad42b086f961b0830833829abc8941
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2019-06-25 10:29:03 -0400

    Add files via upload

---
+++ EP21_PlayerLevelUp/Scripts_01/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public PlayerStatus playerStatus;
+
+    public Text nameText, levelText, currentExpText;
+    public Text hpText, mpText;
+    public Text attackText, defenseText;
+
+    public Text nextToLevelText;
+
+    private void Start()
+    {
+        UpdatePlayerStatus();
+    }
+
+    public void UpdatePlayerStatus()
+    {
+        nameText.text = playerStatus.playerName;
+        levelText.text = "" + playerStatus.playerLevel;
+        currentExpText.text = "" + playerStatus.currentExp;
+
+        hpText.text = "" + playerStatus.currentHp + "/" + playerStatus.maxHp;
+        mpText.text = "" + playerStatus.currentMp + "/" + playerStatus.maxMp;
+
+        attackText.text = "" + playerStatus.attack;
+        defenseText.text = "" + playerStatus.defense;
+
+        nextToLevelText.text = playerStatus.nextLevelExp[playerStatus.playerLevel].ToString();
+    }
+
+}
